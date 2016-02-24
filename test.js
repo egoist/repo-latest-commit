5a03eef4b8cbe5fd2fa255bc5395ef576581bb84
EGOIST <0x142857@gmail.com>
2016-02-24 13:14:37 +0800

    fix test

--- test.js
+++ test.js
@@ -4,7 +4,16 @@ import child from 'child_process'
 
 test('main', t => {
 	try {
-		child.execSync('mkdir -p temp && cd temp && touch what && git init && git add -A && git commit -m "fuck"')
+		child.execSync(`
+			mkdir -p temp
+			cd temp
+			touch what
+			git init
+			git config user.email "you@example.com"
+	  	git config user.name "Your Name"
+			git add -A
+			git commit -m "fuck"
+		`)
 		t.is(commit('temp')[1], 'fuck')
 		child.execSync('rm -rf temp')
 	} catch (e) {
