736050da094eaab7bff3004f326fcf43256639ca
EGOIST <0x142857@gmail.com>
2016-02-24 14:26:15 +0800

    fix lint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var execSync = require('child_process').execSync
 
 module.exports = function (cwd) {
 	var command = 'git log --pretty=oneline'
-	var result = execSync(command, { cwd: cwd }).toString()
+	var result = execSync(command, {cwd: cwd}).toString()
 	return result
 		.split('\n')[0]
 		.split(' ')
