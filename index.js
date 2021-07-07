b25dee3d39ebed85fd00390ea33fb375fd35c015
Nazar Lysyi <n.lysyi@crowdin.com>
2021-07-07 15:02:42 +0300

    fix commit message parsing

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const split = require('split-on-first-occurrence')
 module.exports = function (cwd) {
   const output = split(execSync('git log -n 1', {cwd}).toString(), '\n\n')
 
-  const info = output[0]
+  const infoObject = {}
+
+  output[0]
     .split('\n')
-    .map(line => split(line, ' '))
+    .map(line => {
+      const data = split(line, ' ')
+      infoObject[`${data[0]}`] = data[1]
+      return null
+    })
 
   const result = {message: output[1].trim()}
-  result.commit = info[0][1]
-  result.author = info[1][1].match(/([^<]+)/)[1].trim()
-  result.email = info[1][1].match(/<([^>]+)>/)[1]
-  result.date = info[2][1].trim()
+  result.commit = infoObject.commit
+  result.author = infoObject['Author:'].match(/([^<]+)/)[1].trim()
+  result.email = infoObject['Author:'].match(/<([^>]+)>/)[1]
+  result.date = infoObject['Date:'].trim()
 
   return result
 }
